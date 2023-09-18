6f026e784f1316f184907e6e566a9f1740a03bc8
HanSeongLee <jkhanseong@gmail.com>
2023-09-18 23:42:18 +0900

    style: add instrument sans font

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,8 +1,21 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { Instrument_Sans } from '@next/font/google';
+
+const instrumentSansFont = Instrument_Sans({
+  weight: ['400', '600', '700'],
+  subsets: ['latin'],
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <>
+    <style jsx global>{`
+      :root {
+        --instrument-sans-font: ${instrumentSansFont.style.fontFamily};
+      }
+    `}</style>
+    <Component {...pageProps} />
+  </>;
 }
 
 export default MyApp
