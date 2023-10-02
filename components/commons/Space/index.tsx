2f53dfb8c1c1312b5ab46c0996c05292e22f3580
HanSeongLee <jkhanseong@gmail.com>
2023-10-02 18:20:45 +0900

    feat: update Space component

--- components/commons/Space/index.tsx
+++ components/commons/Space/index.tsx
@@ -4,6 +4,8 @@ import cn from 'classnames';
 
 interface IProps extends HTMLAttributes<HTMLDivElement> {
     direction?: 'vertical' | 'horizontal';
+    align?: 'start' | 'center' | 'end';
+    justify?: 'start' | 'center' | 'end' | 'space-between' | 'space-around';
     gap?: number | {
         row: number;
         column: number;
@@ -24,23 +26,26 @@ interface IProps extends HTMLAttributes<HTMLDivElement> {
 }
 
 const Space: React.FC<IProps> = ({
-                                     direction = 'vertical', gap, responsiveGap, wrap,
-                                     className, children, ...props
+                                     direction = 'vertical', align, justify, gap,
+                                     responsiveGap, wrap, className, children,
+                                     ...props
                                  }) => {
     return (
         <div className={cn(styles.space, className)}
              style={{
                  flexDirection: direction === 'vertical' ? 'column' : 'row',
+                 alignItems: align ? align : undefined,
+                 justifyContent: justify ? justify : undefined,
                  gap: gap ?
                      typeof gap === 'number' ?
                          `${gap}px` : `${gap.row}px ${gap.column}px` : undefined,
                  flexWrap: wrap ? 'wrap' : undefined,
-                '--responsive-gap-row-sm': responsiveGap?.row?.sm ? `${responsiveGap.row.sm}px` : undefined,
-                '--responsive-gap-row-md': responsiveGap?.row?.md ? `${responsiveGap.row.md}px` : undefined,
-                '--responsive-gap-row-lg': responsiveGap?.row?.lg ? `${responsiveGap.row.lg}px` : undefined,
-                '--responsive-gap-column-sm': responsiveGap?.column?.sm ? `${responsiveGap.column.sm}px` : undefined,
-                '--responsive-gap-column-md': responsiveGap?.column?.md ? `${responsiveGap.column.md}px` : undefined,
-                '--responsive-gap-column-lg': responsiveGap?.column?.lg ? `${responsiveGap.column.lg}px` : undefined,
+                 '--responsive-gap-row-sm': responsiveGap?.row?.sm ? `${responsiveGap.row.sm}px` : undefined,
+                 '--responsive-gap-row-md': responsiveGap?.row?.md ? `${responsiveGap.row.md}px` : undefined,
+                 '--responsive-gap-row-lg': responsiveGap?.row?.lg ? `${responsiveGap.row.lg}px` : undefined,
+                 '--responsive-gap-column-sm': responsiveGap?.column?.sm ? `${responsiveGap.column.sm}px` : undefined,
+                 '--responsive-gap-column-md': responsiveGap?.column?.md ? `${responsiveGap.column.md}px` : undefined,
+                 '--responsive-gap-column-lg': responsiveGap?.column?.lg ? `${responsiveGap.column.lg}px` : undefined,
              } as React.CSSProperties}
              {...props}
         >
