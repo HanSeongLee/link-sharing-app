4377f193ab8345daef3adf1d4dfcc294d9047ceb
HanSeongLee <jkhanseong@gmail.com>
2023-10-08 17:43:28 +0900

    fix: correct missing SessionProvider in _app.page.tsx

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,20 +1,23 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { Instrument_Sans } from '@next/font/google';
+import { SessionProvider } from 'next-auth/react';
 
 const instrumentSansFont = Instrument_Sans({
   weight: ['400', '600', '700'],
   subsets: ['latin'],
 });
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps} }: AppProps) {
   return <>
     <style jsx global>{`
       :root {
         --instrument-sans-font: ${instrumentSansFont.style.fontFamily};
       }
     `}</style>
-    <Component {...pageProps} />
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
   </>;
 }
 
