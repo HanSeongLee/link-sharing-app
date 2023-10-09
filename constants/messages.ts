61b3dcc8eca7937ba42e942e1e883f8a6769c0d5
HanSeongLee <jkhanseong@gmail.com>
2023-10-09 17:59:51 +0900

    feat: add error and info messages constants

---
+++ constants/messages.ts
@@ -0,0 +1,9 @@
+export enum ERROR_MESSAGES {
+    REQUIRE_FILED = `Can't be empty`,
+    PASSWORD_NOT_MATCH = `Please check again`,
+    SOMETHING_WENT_WRONG = `Something went wrong. Please try again!`,
+}
+
+export enum INFO_MESSAGES {
+    CHANGED_SAVE = `Your changes have been successfully saved!`,
+}
