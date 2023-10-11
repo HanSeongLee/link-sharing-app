681fd8101243690a3b6906aa3208577c27804ab6
HanSeongLee <jkhanseong@gmail.com>
2023-10-11 18:28:15 +0900

    fix: resolve hydration error from useMediaQueries hook

--- hooks/useMediaQueries.ts
+++ hooks/useMediaQueries.ts
@@ -1,9 +1,23 @@
+import { useEffect, useState } from 'react';
 import { useMediaQuery } from 'usehooks-ts';
 
 export const useMediaQueries = () => {
+    const [isClient, setIsClient] = useState(false)
     const mobileQuery = useMediaQuery('(max-width: 767px)');
-    const tabletQuery = useMediaQuery('(min-width: 767px) and (max-width: 1339px)');
-    const desktopQuery = useMediaQuery('(min-width: 1339px)');
+    const tabletQuery = useMediaQuery('(min-width: 767px) and (max-width: 1399px)');
+    const desktopQuery = useMediaQuery('(min-width: 1399px)');
+
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
+
+    if (!isClient) {
+        return {
+            mobileQuery: false,
+            tabletQuery: false,
+            desktopQuery: false,
+        };
+    }
 
     return {
         mobileQuery,
