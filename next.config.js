9fde273bfff4dd0170e36da517d2437099e12837
HanSeongLee <jkhanseong@gmail.com>
2023-10-14 17:53:13 +0900

    feat: remove root page and redirect root path to editor page

--- next.config.js
+++ next.config.js
@@ -25,6 +25,15 @@ const nextConfig = {
     // for more information.
     hideSourceMaps: true,
   },
+  redirects: async () => {
+    return [
+      {
+        source: '/',
+        destination: '/editor',
+        permanent: false,
+      },
+    ]
+  }
 }
 
 module.exports = withSentryConfig(
